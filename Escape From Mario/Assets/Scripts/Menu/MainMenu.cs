9414cc71ce772be116fd990491262837e1d8e239
engin  can <altinozengincan@gmail.com>
2023-01-18 11:45:30 +0300

    Small Changes

--- Escape From Mario/Assets/Scripts/Menu/MainMenu.cs
+++ Escape From Mario/Assets/Scripts/Menu/MainMenu.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    void Update()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
     public void OnStartScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
